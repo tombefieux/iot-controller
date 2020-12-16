3fb9ed52c1e013c67ebed03196e00ae9dc62d8ca
Tom BEFIEUX <tom.befieux@gmail.com>
2020-12-16 13:09:10 +0100

    new alarm melodie

--- Alarm.h
+++ Alarm.h
@@ -13,10 +13,4 @@ private:
     static void runAlarm(void*);
 
     bool alarmAlreadyLunched = false;
-
-    // do, do#, ré, ré#, mi, fa, fa#, sol, sol#, la, la#, si
-    const static float note[12];
-    static const int nombreDeNotes = 32;
-    static const int tempo = 150;
-    static const int melodie[][3];
 }; 
